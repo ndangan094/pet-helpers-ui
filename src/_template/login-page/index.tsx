c9b8006460a728cca64928979984137970ad3274
vinhspm <42412632+vinhspm@users.noreply.github.com>
2021-07-22 14:58:20 +0700

    again :>

--- src/_template/login-page/index.tsx
+++ src/_template/login-page/index.tsx
@@ -1,19 +1,19 @@
 import Header from "../../_layout/header";
 import Footer from "../../_layout/footer";
-import {ContainerCompopnent, FormComponent} from './styled-components';
+// import {ContainerCompopnent, FormComponent} from './styled-components';
 
 const LoginPageTemplate = () => {
     return (
         <>
             <Header/>
-            <ContainerCompopnent>
-              <FormComponent>
+            <div>
+              <form>
                 <label htmlFor="username">Username or Email</label>
                 <input name='username' type='text' placeholder='username or email'/>
                 <label htmlFor="password">Password</label>
                 <input name='password' type='password' placeholder='password' />
-              </FormComponent>
-            </ContainerCompopnent> 
+              </form>
+            </div> 
            
             <Footer/>
         </>
