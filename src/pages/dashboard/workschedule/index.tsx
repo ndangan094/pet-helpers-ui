a373e9a3b792f6e55f80755b7ef4ca94b2ac3ce8
vinhspm <42412632+vinhspm@users.noreply.github.com>
2021-08-08 10:03:59 +0700

    check schedule and regis new schedule
    login to volunteer to use this function

---
+++ src/pages/dashboard/workschedule/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import WorkTemplate  from  '../../../_template/work-page/index'
+
+const DashBoard = () => {
+    return (
+        <>
+            <Head>
+                <title>Work Schedule Manager</title>
+            </Head>
+            <WorkTemplate/>
+        </>
+    )
+}
+
+
+export default DashBoard;
